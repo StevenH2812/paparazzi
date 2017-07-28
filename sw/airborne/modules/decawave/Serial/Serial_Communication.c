1be89fdff13372ea83a7801b20c4a502b321e41b
StevenH2812 <stevenhelm@live.nl>
2017-07-28 14:48:09 +0200

    new airframe files

--- sw/airborne/modules/decawave/Serial/Serial_Communication.c
+++ sw/airborne/modules/decawave/Serial/Serial_Communication.c
@@ -37,19 +37,19 @@
 
 // Serial Port
 #include "mcu_periph/uart.h"
-PRINT_CONFIG_VAR(STEREO_UART)
-PRINT_CONFIG_VAR(STEREO_BAUD)
+PRINT_CONFIG_VAR(SERIAL_UART)
+PRINT_CONFIG_VAR(SERIAL_BAUD)
 
 // define coms link for stereocam
-#define STEREO_PORT   (&((STEREO_UART).device))
-struct link_device *xdev = STEREO_PORT;
+#define SERIAL_PORT   (&((SERIAL_UART).device))
+struct link_device *xdev = SERIAL_PORT;
 
-#define StereoGetch() STEREO_PORT ->get_byte(STEREO_PORT->periph)
-#define StereoSend1(c) STEREO_PORT->put_byte(STEREO_PORT->periph, 0, c)
-#define StereoUartSend1(c) StereoSend1(c)
-#define StereoSend(_dat,_len) { for (uint8_t i = 0; i< (_len); i++) StereoSend1(_dat[i]); };
-#define StereoUartSetBaudrate(_b) uart_periph_set_baudrate(STEREO_PORT, _b);
-#define StereoChAvailable()(xdev->char_available(xdev->periph))
+#define SerialGetch() SERIAL_PORT ->get_byte(SERIAL_PORT->periph)
+#define SerialSend1(c) SERIAL_PORT->put_byte(SERIAL_PORT->periph, 0, c)
+#define SerialUartSend1(c) SerialSend1(c)
+#define SerialSend(_dat,_len) { for (uint8_t i = 0; i< (_len); i++) SerialSend1(_dat[i]); };
+#define SerialUartSetBaudrate(_b) uart_periph_set_baudrate(SERIAL_PORT, _b);
+#define SerialChAvailable()(xdev->char_available(xdev->periph))
 
 
 
@@ -70,18 +70,18 @@ struct link_device *xdev = STEREO_PORT;
 
 
 // Module data
-struct AvoidNavigationStruct {
+struct DataStruct {
 	//  uint8_t mode; ///< 0 = straight, 1 =  right, 2 = left, ...
 	uint8_t decode_cnt;
-	uint8_t stereo_bin[8];
+	uint8_t bin[8];
 	uint8_t timeout;
 };
 
-struct AvoidNavigationStruct avoid_navigation_data;
+struct DataStruct serial_data;
 
 
 
-static int stereo_range_measurement = 0;
+static int range_measurement = 0;
 
 
 
@@ -91,25 +91,25 @@ static int stereo_parse(uint8_t c)
 
 	//printf("%c",c);
 	//return;
-    avoid_navigation_data.timeout = 20;
+    serial_data.timeout = 20;
 
 	// arduino: printf("%05d\n"); in mm
-	if ((c == '\n') || (avoid_navigation_data.decode_cnt > 7))
+	if ((c == '\n') || (serial_data.decode_cnt > 7))
 	{
-		avoid_navigation_data.decode_cnt = 0;
+		serial_data.decode_cnt = 0;
 		return -1;
 	}
   // Protocol is one byte only: store last instance
-  avoid_navigation_data.stereo_bin[avoid_navigation_data.decode_cnt] = c;
-  avoid_navigation_data.decode_cnt++;
+  serial_data.bin[serial_data.decode_cnt] = c;
+  serial_data.decode_cnt++;
 
-  if (avoid_navigation_data.decode_cnt == 5)
+  if (serial_data.decode_cnt == 5)
   {
 	  // decode: in de buffer staan nu 5 ASCII character van de afstand, bijvoorbeeld '0','0','0','2','2'
 	  //Strings zijn char arrays die eindigen met 0 in C
-	  avoid_navigation_data.stereo_bin[5] = 0;
-	  stereo_range_measurement = atoi((char*)avoid_navigation_data.stereo_bin);
-	  avoid_navigation_data.decode_cnt = 0;
+	  serial_data.bin[5] = 0;
+	  range_measurement = atoi((char*)serial_data.bin);
+	  serial_data.decode_cnt = 0;
 	  return 1;
 
   }
@@ -119,41 +119,41 @@ static int stereo_parse(uint8_t c)
 }
 
 
-void stereocam_droplet_init(void)
+void decawave_serial_init(void)
 {
   // Do nothing
-	  avoid_navigation_data.decode_cnt = 0;
-	  avoid_navigation_data.timeout = 0;
+	  serial_data.decode_cnt = 0;
+	  serial_data.timeout = 0;
 
 	  //uart_periph_set_baudrate(&uart1,B9600);
 }
-void stereocam_droplet_periodic(void)
+void decawave_serial_periodic(void)
 {
 	int newrange = 0;
 
 	// Read Serial
-	while (StereoChAvailable()) {
-		if (stereo_parse(StereoGetch()) > 0)
+	while (SerialChAvailable()) {
+		if (stereo_parse(SerialGetch()) > 0)
 			newrange++;
 	}
 
-	if (avoid_navigation_data.timeout <= 0)
+	if (serial_data.timeout <= 0)
 		return;
 
 	if (newrange > 0)
 	{
 		uint8_t a,b;
 		char buf[256];
-		sprintf(buf,"Range = %d", stereo_range_measurement);
+		sprintf(buf,"Range = %d", range_measurement);
 
-		a = stereo_range_measurement;
+		a = range_measurement;
 		b=0;
 		// Results
 		//DOWNLINK_SEND_DEBUG(DefaultChannel, DefaultDevice, strlen(buf), (uint8_t*) buf);
 		DOWNLINK_SEND_DEBUG_MCU_LINK(DefaultChannel, DefaultDevice, &a, &b,&b);
 	}
 
-	avoid_navigation_data.timeout --;
+	serial_data.timeout --;
 
 
 }
--- sw/airborne/modules/decawave/Serial/Serial_Communication.h
+++ sw/airborne/modules/decawave/Serial/Serial_Communication.h
@@ -8,8 +8,8 @@
 #ifndef SW_AIRBORNE_MODULES_DECAWAVE_SERIAL_SERIAL_COMMUNICATION_H_
 #define SW_AIRBORNE_MODULES_DECAWAVE_SERIAL_SERIAL_COMMUNICATION_H_
 
-extern void stereocam_droplet_init(void);
-extern void stereocam_droplet_periodic(void);
+extern void decawave_serial_init(void);
+extern void decawave_serial_periodic(void);
 
 
 
